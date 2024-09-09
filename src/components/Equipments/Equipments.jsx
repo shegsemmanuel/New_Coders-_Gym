9d82fc90e2d77ac74938f450bb0f01496e0c3ed6
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-09-09 03:18:24 +0100

    Equipment section

---
+++ src/components/Equipments/Equipments.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { GrYoga } from 'react-icons/gr';
+import { FaDumbbell } from 'react-icons/fa6';
+import { GiGymBag } from 'react-icons/gi';
+
+
+
+const EquipmentData = [
+    {
+        id:1,
+        title:"Yoga Equipments",
+        desc:"It is a long established fact that a reader readable.",
+        icon: <GrYoga />,
+        delay: 0.3, 
+    },
+    {
+        id: 2,
+        title: "Muscles Equipments",
+        desc: "It is a long established fact that a reader readable.",
+        link: "/",
+        icon: <FaDumbbell />,
+        delay: 0.6,
+    },
+    {
+        id: 3,
+        title: "Fitness Equipments",
+        desc: "It is a long established fact that a reader readable.",
+        link: "/",
+        icon: <GiGymBag />,
+        delay: 0.9,
+    },
+];
+
+const Equipments = () => {
+  return (
+    <div>
+        <div className="container py-24">
+           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 font-playfair">
+             <div className="space-y-4 p-6">
+                <h1 className="text-3xl md:text-4xl font-bold">What we offer for you</h1>
+                <p className="text-gray-500">It is a long established fact that a reader readable.</p>
+             </div>
+             { EquipmentData.map((item) => {
+                return (
+                    <div className="space-y-4 p-6 bg-[#fbfbfb] hover:bg-white rounded-xl hover:shadow-[0_0_22px_0_rgba(0,0,0,0.15)]" >
+                        <div className="text-4xl">{item.icon}</div>
+                        <p className="text-2xl font-semibold">{item.title}</p>
+                        <p className="text-gray-500">{item.desc}</p>
+                    </div>
+                );
+             })}
+           </div>
+        </div>
+    </div>
+  );
+};
+
+export default Equipments
