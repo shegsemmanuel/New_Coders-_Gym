959bfcd96c83940da1ad721238c4e6353a92200b
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-09-09 20:26:56 +0100

    tab section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Banner from "./components/Banner/Banner";
 
 import Img1 from "./assets/2.png";
 import Img2 from "./assets/3.png";
+import TabComp from "./components/Tab/TabComp";
 
 const BannerData = {
   image: Img1,
@@ -41,7 +42,7 @@ const App = () => {
       </div>
       <Equipments/>
       <Banner {...BannerData} />
-      
+      <TabComp />
       <Banner {...Banner2Data} />
     </div>  
   );
